c9966189028a08f273cd6541122da8aaf8215134
Iris Cheung <hokyinirischeung@gmail.com>
2026-02-06 22:55:49 -0800

    debug3

--- src/components/BenefitsList.tsx
+++ src/components/BenefitsList.tsx
@@ -29,8 +29,7 @@ const BenefitsList: React.FC<BenefitsListProps> = ({ userCardId }) => {
         if (!userCardId || userCardId === "{userCardId}") return;
 
         setLoading(true);
-        console.log({userCardId})
-        fetch(`${backendUrl}/card/{userCardId}`)
+        fetch(`${backendUrl}/card/${userCardId}`)
             .then(res => res.json())
             .then(data => {
                 if (Array.isArray(data)) {
